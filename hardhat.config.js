a85f91f0023c2edaca5a153217899f399b86e795
Dhruv Gupta <dhruv035@gmail.com>
2023-10-18 11:51:41 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,17 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.21",
+  etherscan:{
+    apiKey: process.env.ETHERSCAN,
+  },
+  networks:{
+    goerli:{
+      url:process.env.GOERLI_RPC,
+      accounts:[process.env.PRIVATE_KEY]
+    },
+    mumbai:{
+      url:process.env.MUMBAI_RPC,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  }
 };
